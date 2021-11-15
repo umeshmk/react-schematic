23b99991603c6e261a4f04bc312ebebd2a8d2b6b
umesh kadam <umeshk149@gmail.com>
2021-11-15 21:21:23 +0530

    deleted oldElements folder"

--- oldElements/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './Button';
-export * from './Container';
-export * from './flex/Flex';
-export * from './flex/Item';
-export * from './Grid';
-export * from './Modal';
-export * from './Typography';
