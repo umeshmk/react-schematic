0312ed789b9ab19d6b49dbf4bfdabd8585d4389c
umesh kadam <umeshk149@gmail.com>
2021-11-16 19:54:06 +0530

    added webpack & generated bundle with configs

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: 'node',
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [],
+  optimization: {},
+};
